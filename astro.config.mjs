e9353665c232d9c9a2176e1e6382677e12f0b2ea
shadow1982 <40671364+shadow1982@users.noreply.github.com>
2025-06-16 18:37:50 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://sombranew.github.io/",
   integrations: [
     sitemap(),
     robotsTxt({
